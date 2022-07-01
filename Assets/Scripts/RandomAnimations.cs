812d2b4c3daafbac461499a40e3e852c16413734
anmolbajpai24 <anmolbajpai24@gmail.com>
2022-07-02 03:47:01 +0530

    Added Animations

--- Assets/Scripts/RandomAnimations.cs
+++ Assets/Scripts/RandomAnimations.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class RandomAnimations : MonoBehaviour
 {
+    private Animator anim;
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
-        
+        anim = GetComponent<Animator>();
+
+        while(true)
+        {
+            yield return new WaitForSeconds(1);
+
+            anim.SetInteger(("JumpIndex"),Random.Range(0, 6));
+            
+        }
     }
 
     // Update is called once per frame
