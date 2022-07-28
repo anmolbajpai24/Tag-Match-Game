ec1fb713699af2df19309c5ed713b3ffee828b56
anmolbajpai24 <anmolbajpai24@gmail.com>
2022-07-28 23:25:55 +0530

    Changed Tag board from one character to another

--- Assets/ChangeImage.cs
+++ Assets/ChangeImage.cs
@@ -5,16 +5,22 @@ using UnityEngine.UI;
 
 public class ChangeImage : MonoBehaviour
 {
-    public Image image;
+    public Image imageOnBlue;
+    public Image imageOnRed;
     public Sprite tagged;
     public Sprite tag;
     // Start is called before the first frame update
     IEnumerator Start()
     {
-      // new WaitForSeconds(10);
-        image.sprite = tag;
+        // new WaitForSeconds(10);
+        imageOnRed.enabled = false;
+        imageOnBlue.enabled = true;
+        imageOnBlue.sprite = tag;
         yield return new WaitForSeconds(3f);
-        image.sprite = tagged;
+
+        imageOnBlue.enabled = false;
+        imageOnRed.enabled = true;
+        imageOnRed.sprite = tagged;
     }
 
     // Update is called once per frame
